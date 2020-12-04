5d1ceb86d39d681a7442cf25391756b98708853a
Christopher Dehner <topherdehner@gmail.com>
2020-12-04 01:21:02 +0000

    Update to show floats issue

--- class_files/1/floats/floats.c
+++ class_files/1/floats/floats.c
@@ -6,5 +6,7 @@ int main(void)
     float x = get_float("x: ");
     float y = get_float("y: ");
 
-    printf("x / y = %f\n", x/y);
+    // not possible for computer to store more than the closest possible match for the operation's result
+    // precision depends on the data type
+    printf("x / y = %.100f\n", x/y);
 }
