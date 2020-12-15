1414eab723bdb1e7336359ea57ea2ff9c37fd5b7
Chris Dehner <topherdehner@gmail.com>
2020-12-14 20:28:07 -0500

    Initial commit

---
+++ pset1/cash/cash.c
@@ -0,0 +1,12 @@
+// get_float to get user's change owed
+// re-prompt if negative
+// convert dollars to cents (float to int)
+// round to nearest penny (eg. round (dollars * 100) to int value)
+// last line = "%i\n"
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+}
