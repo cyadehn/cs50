bd02ffd7e0e11c4d6a5cd59074d3666060340583
Christopher Dehner <topherdehner@gmail.com>
2020-12-16 23:11:55 +0000

    Corrected formatting

--- pset1/cash/cash.c
+++ pset1/cash/cash.c
@@ -11,22 +11,31 @@ int GetCoinNum();
 
 int main(void)
 {
-  float input = 0; 
-  while (!(input > 0))
-  {
-    input = get_float("Please enter the change owed (ex. '$$.CC'): ");
-  }
-  int cents = (input * 100);
-  int coinNum = GetCoinNum(cents);
-  printf("%i\n", coinNum);
+    float input = 0;
+    while (!(input > 0))
+    {
+        input = get_float("Please enter the change owed (ex. '$$.CC'): ");
+    }
+    int cents = (input * 100);
+    int coinNum = GetCoinNum(cents);
+    printf("%i\n", coinNum);
 }
 
 //Recursively get cents as the passed values become less and less
 //Recursion terminates when the value passed is less than 5 (meaning they just become pennies/cents!)
 int GetCoinNum(int cents)
 {
-    if (cents < 5) return cents;
-    if (cents < 10) return (cents / 5) + GetCoinNum(cents % 5);
-    if (cents < 25) return (cents / 10) + GetCoinNum(cents % 10);
+    if (cents < 5)
+    {
+        return cents;
+    }
+    if (cents < 10)
+    {
+        return (cents / 5) + GetCoinNum(cents % 5);
+    }
+    if (cents < 25)
+    {
+        return (cents / 10) + GetCoinNum(cents % 10);
+    }
     return (cents / 25) + GetCoinNum(cents % 25);
-  }
+}
