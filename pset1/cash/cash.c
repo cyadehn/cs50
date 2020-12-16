8dd49272c6e39825cbf3ae7b0f84795ca7cd86ab
Chris Dehner <topherdehner@gmail.com>
2020-12-16 17:30:53 -0500

    Update pseudocode

--- pset1/cash/cash.c
+++ pset1/cash/cash.c
@@ -2,6 +2,7 @@
 // re-prompt if negative
 // convert dollars to cents (float to int)
 // round to nearest penny (eg. round (dollars * 100) to int value)
+// implement greedy algorithm
 // last line = "%i\n"
 #include <cs50.h>
 #include <stdio.h>
