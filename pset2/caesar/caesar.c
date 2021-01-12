a053d04f70cea0973bd94f6bd5675be07c82d385
Chris Dehner <topherdehner@gmail.com>
2021-01-11 19:48:20 -0500

    Initial commit / pseudocode

---
+++ pset2/caesar/caesar.c
@@ -0,0 +1,12 @@
+// TODO: accept a single command-line argument (non-negative integer)
+int main(void)
+{
+    // TODO: check if user entered exactly 1 command-line arguments
+    // TODO: check if user key contains only real numbers
+    // TODO: convert key to integer
+    // TODO: print plaintext prompt to get string from user
+    // TODO: iterate over plaintext, do for each character:
+        // rotate and print character, if uppercase or lowercase alphabetical char
+        // print original character if neither upper or lowercase
+    // TODO: print a newline + return 0
+}
