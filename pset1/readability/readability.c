f5f6b7a15149ade51e934e90f9e6899b8020ada2
Chris Dehner <topherdehner@gmail.com>
2021-01-31 17:49:36 -0500

    Initial commit

---
+++ pset1/readability/readability.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    // TODO: Prompt user for string of text
+    //
+    // TODO: Count number of letters, words, and sentences (ending in . ! ?)
+    //
+    // TODO: Determine grade level (rounded to nearest integer) using the Coleman-Liau formula:
+    // index = 0.0588 * L - 0.296 * S - 15.8
+    // A grade higher than 16 should output "16+", lower than 1 is "Before Grade 1"
+    //
+    // TODO: Print output as "Grade X"
+}
