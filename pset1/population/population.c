b4ffd4ef66caf7c71776820c4e1816ee453f16b1
Chris Dehner <topherdehner@gmail.com>
2021-01-31 17:09:55 -0500

    Replace todos

--- pset1/population/population.c
+++ pset1/population/population.c
@@ -3,19 +3,19 @@
 
 int main(void)
 {
-    // TODO: Prompt for start size
+    // DONE: Prompt for start size
     int startingSize = get_int("Start size: ");
     while (startingSize < 9)
     {
         startingSize = get_int("Start size: ");
     } 
     int endingSize = get_int("End size: ");
-    // TODO: Prompt for end size
+    // DONE: Prompt for end size
     while (endingSize < startingSize)
     {
         endingSize = get_int("End size: ");
     } 
-    // TODO: Calculate number of years until we reach threshold
+    // DONE: Calculate number of years until we reach threshold
     int years = 0;
     int p = startingSize;
     while (p < endingSize)
@@ -24,6 +24,6 @@ int main(void)
         // Each year, p/3 llamas are born and p/4 llamas die
         p = p + (p/3) - (p/4);
     }
-    // TODO: Print number of years
+    // DONE: Print number of years
     printf("Years: %i\n", years);
 }
