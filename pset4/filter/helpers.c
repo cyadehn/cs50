ca1fe10d7de373f1a25c6c201648cfb2cf3efdaa
Chris Dehner <topherdehner@gmail.com>
2021-02-14 18:56:49 -0500

    Write initial pseudocode

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -3,23 +3,32 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Iterate over rows
+    // Average BRG values + assign to each byte
     return;
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Iterate over image rows
+    // Iterate over row pixels until 1/2-way thru + swap with WIDTH - ith item
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Iterate over image rows
+    // Iterate over row pixels + average with surrounding values
     return;
 }
 
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Iterate over rows
+    // Iterate over row pixels + apply image processing matrices
+    // Calculate Sobel value and assign to output stream
     return;
 }
