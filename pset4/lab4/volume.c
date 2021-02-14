eabfc23984b6ead626b2d48a272ba198181e6911
Chris Dehner <topherdehner@gmail.com>
2021-02-14 12:14:42 -0500

    Read/write header to output file

--- pset4/lab4/volume.c
+++ pset4/lab4/volume.c
@@ -34,12 +34,17 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-    // For size of file header in bytes, read until header end and write to output
-    // I coooooould do this in one step by saying 'I want 44 uint8_t'... but I'd have to make my own type that's 44 bits long or... WAIT I CAN USE MALLOC RIGHT?
-    uint8_t b = 0;
-    for (int i = 0; i < HEADER_SIZE; i++)
+    // Allocate mem for header, write to output file
+    uint8_t *header[HEADER_SIZE] = malloc(HEADER_SIZE);
+    // Check if pointer is null
+    if (header == NULL)
     {
-        fread(&b, sizeof(uint8_t), 1, )
+        return 1;
+    }
+    else
+    {
+        fwrite(header, sizeof(uint8_t), HEADER_SIZE, output);
+        free(header);
     }
     // TODO: Read samples from input file and write updated data to output file
 
